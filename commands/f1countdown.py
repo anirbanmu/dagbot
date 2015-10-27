b332a4adf261ddcc7b14526c0e639e2ddf0c2abc
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2015-10-27 00:15:43 -0400

    Added countdown to next Formula 1 session/qualifying/race as a command

---
+++ commands/f1countdown.py
@@ -0,0 +1,79 @@
+import pytz
+from urllib2 import urlopen
+from datetime import datetime, timedelta
+from icalendar import Calendar, Event
+
+def get_raw_events(url):
+  file = urlopen(url)
+  contents = file.read()
+  file.close()
+  return contents
+
+def sanitize_dt(dt):
+    # Assume UTC for non tz aware events
+    newdt = dt if hasattr(dt, 'hour') else datetime.combine(dt, datetime.min.time()).replace(tzinfo=pytz.UTC)
+    newdt = newdt if newdt.tzinfo != None else newdt.replace(tzinfo=pytz.UTC)
+    return newdt
+
+# Conservative delta used. No event should be longer than 1 week.
+def prune_event_start(start_time, utc_now):
+    delta = utc_now - sanitize_dt(start_time)
+    return delta > timedelta(weeks=1)
+
+def prune_event_end(end_time, utc_now):
+    return sanitize_dt(end_time) < utc_now
+
+def prune_past_events(ics_events, now):
+    utc_now = now.replace(tzinfo=pytz.UTC)
+    events = []
+    for component in ics_events.walk():
+            if component.name == "VEVENT":
+                end = component.get('dtend')
+                if prune_event_start(component.get('dtstart').dt, utc_now) or (end and prune_event_end(end.dt, utc_now)):
+                    continue
+                events.append(component)
+    return events
+
+class FormulaOneCountdown():
+    # Calendar from http://www.f1fanatic.co.uk/contact/f1-fanatic-calendar/
+    calendar_url = 'https://www.google.com/calendar/ical/hendnaic1pa2r3oj8b87m08afg%40group.calendar.google.com/public/basic.ics'
+    update_interval = timedelta(days=1)
+    keywords = ['@countdown', '@next']
+    modifiers = ['r', 'q']
+    modifier_filters = {'': '', 'r': 'grand prix', 'q': 'grand prix qualifying'}
+    def __init__(self):
+        self.events = []
+        self.last_updated = datetime.min
+        self.update_calendar()
+
+    def update_calendar(self):
+        if datetime.utcnow() - self.last_updated > self.update_interval:
+            self.get_new_calendar()
+
+    def get_new_calendar(self):
+        self.last_updated = datetime.utcnow()
+        self.events = prune_past_events(Calendar.from_ical(get_raw_events(self.calendar_url)), self.last_updated)
+
+    def closest_event(self, event_type_end):
+        deltas = []
+        utc_now = sanitize_dt(datetime.utcnow())
+        for event in self.events:
+            delta = sanitize_dt(event.get('dtstart').dt) - utc_now
+            if (event.get('summary').lower().endswith(event_type_end) and delta > timedelta(microseconds=0)):
+                deltas.append((event, delta))
+        deltas.sort(key=lambda x: x[1])
+        if (len(deltas) == 0):
+            return None
+        return deltas[0]
+
+    def command_response(self, command_modifier):
+        self.update_calendar()
+        event = self.closest_event(self.modifier_filters[command_modifier])
+        if not event:
+            return 'No future event found'
+
+        delta = event[1]
+        hours, remainder = divmod(delta.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        response = '%s starting in %d %s %02d:%02d:%02d' % (event[0].get('summary'), delta.days, 'days' if delta.days != 1 else 'day', hours, minutes, seconds)
+        return response.encode('ascii')
