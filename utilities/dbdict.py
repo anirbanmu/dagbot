826dd24af0ec45b3ec3d7bb26678ce99dcb9d5b4
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2015-11-06 20:30:04 -0500

    Various small improvements. Switched from storing list of tuple(word, count) to dict of word -> count.

--- utilities/dbdict.py
+++ utilities/dbdict.py
@@ -1,19 +1,23 @@
-import sqlite3, pickle, json
+import sqlite3, pickle
+from base64 import b64encode, b64decode
 
 def to_db(data):
-    return pickle.dumps(data, pickle.HIGHEST_PROTOCOL).encode('base64')
+    return b64encode(pickle.dumps(data, pickle.HIGHEST_PROTOCOL))
 
 def from_db(data):
-    return pickle.loads(data.decode('base64'))
+    return pickle.loads(b64decode(data))
 
 class DatabaseDictionary(object):
     def __init__(self, file_name):
-        self.connection = sqlite3.connect(file_name)
+        self.connection = sqlite3.connect(file_name, isolation_level=None)
         self.cursor = self.connection.cursor()
+
+        self.cursor.execute('PRAGMA journal_mode=WAL')
+        self.cursor.execute('PRAGMA synchronous=OFF')
 
         self.cursor.execute(('CREATE TABLE IF NOT EXISTS dictionary ('
                              'id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,'
-                             'key TEXT UNIQUE NOT NULL,'
+                             'key BLOB UNIQUE NOT NULL,'
                              'value BLOB)'))
 
     def get_random_key(self):
@@ -30,6 +34,17 @@ class DatabaseDictionary(object):
             raise KeyError
         return value
 
+    def __delitem__(self, key):
+        self.__getitem__(key) # Will ensure key exists
+        self.cursor.execute('DELETE FROM dictionary WHERE key = ?', (to_db(key),))
+
+    def update(self, other):
+        self.cursor.executemany('INSERT OR REPLACE INTO dictionary VALUES(null, ?, ?)', ((to_db(k), to_db(v)) for k,v in other.iteritems()))
+
+    def replace(self, other):
+        self.cursor.execute('DELETE FROM dictionary')
+        self.cursor.executemany('INSERT INTO dictionary VALUES(null, ?, ?)', ((to_db(k), to_db(v)) for k,v in other.iteritems()))
+
     def get(self, key):
         self.cursor.execute('SELECT value FROM dictionary WHERE key = ?', (to_db(key),))
         value = self.cursor.fetchone()
@@ -41,13 +56,16 @@ class DatabaseDictionary(object):
         return [from_db(v[0]) for v in values]
 
     def key_count(self):
-        self.cursor.execute('SELECT COUNT(*) from dictionary')
+        self.cursor.execute('SELECT COUNT(*) FROM dictionary')
         return self.cursor.fetchone()[0]
 
+    def begin(self):
+        self.cursor.execute('BEGIN')
+
     def commit(self):
-        self.cursor.execute('PRAGMA shrink_memory')
-        self.cursor.execute('VACUUM')
         self.connection.commit()
+        self.cursor.execute('PRAGMA shrink_memory')
 
     def close(self):
+        self.connection.commit()
         self.connection.close()
